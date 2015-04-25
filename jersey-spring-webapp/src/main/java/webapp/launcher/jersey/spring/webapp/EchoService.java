959e59c293ff9c7a757f157c5cfde7383ed639a2
Geng Li <ligeng0420@gmail.com>
2015-04-25 19:48:40 +0100

    Adding example to read from config

--- jersey-spring-webapp/src/main/java/webapp/launcher/jersey/spring/webapp/EchoService.java
+++ jersey-spring-webapp/src/main/java/webapp/launcher/jersey/spring/webapp/EchoService.java
@@ -1,5 +1,7 @@
 package webapp.launcher.jersey.spring.webapp;
 
+import com.google.inject.name.Named;
+
 import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
@@ -8,15 +10,21 @@ import javax.ws.rs.PathParam;
 @Path("test")
 public class EchoService {
     private EchoFunction echoFunction;
+    private String prefix;
 
     @Inject
-    public EchoService(EchoFunction echoFunction) {
+    public EchoService(EchoFunction echoFunction, @Named("test.input") String prefix) {
         this.echoFunction = echoFunction;
+        this.prefix = prefix;
     }
 
     @GET
     @Path("{input}")
     public String echo(@PathParam("input") String input) {
-        return echoFunction.echo(input);
+        return echoFunction.echo(prefix + ":" + input);
+    }
+
+    public void setPrefix(String prefix) {
+        this.prefix = prefix;
     }
 }
