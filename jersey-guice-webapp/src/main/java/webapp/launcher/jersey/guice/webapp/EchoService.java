4a4aef2e11f7cd065413fb001beae0271c55749b
Geng Li <ligeng0420@gmail.com>
2015-04-26 21:29:26 +0100

    Adding example usage of AsyncResponse

--- jersey-guice-webapp/src/main/java/webapp/launcher/jersey/guice/webapp/EchoService.java
+++ jersey-guice-webapp/src/main/java/webapp/launcher/jersey/guice/webapp/EchoService.java
@@ -4,6 +4,8 @@ import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
+import javax.ws.rs.container.AsyncResponse;
+import javax.ws.rs.container.Suspended;
 
 @Path("test")
 public class EchoService {
@@ -18,5 +20,20 @@ public class EchoService {
     @Path("{input}")
     public String echo(@PathParam("input") String input) {
         return echoFunction.echo(input);
+    }
+
+    @GET
+    @Path("wait")
+    public void wait(@Suspended final AsyncResponse asyncResponse) {
+        new Thread(() -> {
+            try {
+                Thread.sleep(5000);
+            } catch (Exception ex) {
+
+            }
+            System.err.println("Just before async finished");
+            asyncResponse.resume("Finished after 5000 ms");
+        }).start();
+        System.err.println("I finish first!");
     }
 }
