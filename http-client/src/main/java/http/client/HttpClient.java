99e88f586c0c96ecc5743b58ba3acaa0fcdf6ce0
Geng Li <ligeng0420@gmail.com>
2015-05-02 22:34:11 +0100

    Adding example for connecting to https

--- http-client/src/main/java/http/client/HttpClient.java
+++ http-client/src/main/java/http/client/HttpClient.java
@@ -1,16 +1,18 @@
 package http.client;
 
 import org.eclipse.jetty.client.api.ContentResponse;
+import org.eclipse.jetty.util.ssl.SslContextFactory;
 
 public class HttpClient {
     public static void main(String[] args) throws Exception {
         org.eclipse.jetty.client.HttpClient httpClient = null;
         try  {
-            httpClient = new org.eclipse.jetty.client.HttpClient();
+            SslContextFactory sslContextFactory = new SslContextFactory();
+            httpClient = new org.eclipse.jetty.client.HttpClient(sslContextFactory);
 
             httpClient.start();
 
-            ContentResponse response = httpClient.GET("http://www.google.co.uk");
+            ContentResponse response = httpClient.GET("https://simple-executor.herokuapp.com/echo");
             System.out.println(response.getContentAsString());
         } finally {
             if (httpClient != null) {
